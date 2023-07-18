fc2035284da79d0aed11e56b20fe7ddf38eb3fe7
Ajay Singh Ahir <138421212+hailace@users.noreply.github.com>
2023-07-18 16:57:32 -0400

    Update artwork.js

--- artwork.js
+++ artwork.js
@@ -1,11 +1,11 @@
-var disqus_config = function () {
-  this.page.url = window.location.href;  // Set the page URL dynamically
-  this.page.identifier = 'enlarged-image-' + Date.now();  // Set a unique identifier for the page
-};
+    var disqus_config = function () {
+      this.page.url = window.location.href;
+      this.page.identifier = "artwork1"; // Replace with a unique identifier for the page
+    };
 
-(function() {
-  var d = document, s = d.createElement('script');
-  s.src = 'https://https-hailace-github-io.disqus.com/embed.js';
-  s.setAttribute('data-timestamp', +new Date());
-  (d.head || d.body).appendChild(s);
-})();
+    (function() {
+      var d = document, s = d.createElement('script');
+      s.src = '//' + 'hailace-github-io' + '.disqus.com/embed.js'; // Replace 'your_disqus_shortname' with your own Disqus shortname
+      s.setAttribute('data-timestamp', +new Date());
+      (d.head || d.body).appendChild(s);
+    })();
